e3d98b5df0bc8e60264c3cda29983fa7b6e6c986
nshib00 <nshib.54321@gmail.com>
2026-04-24 12:05:40 +0300

    Implement password hashing

---
+++ internal/hashpwd/hash.go
@@ -0,0 +1,18 @@
+package hashpwd
+
+import "golang.org/x/crypto/bcrypt"
+
+const bcryptCost = 12
+
+func HashPassword(password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcryptCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hash), nil
+}
+
+func VerifyPassword(password string, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
